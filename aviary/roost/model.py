bc62f01758b7776433a91b9a7c3d3c8242426f7b
muhamad-kurniawan <44334305+muhamad-kurniawan@users.noreply.github.com>
2024-08-16 16:23:52 +0900

    Update model.py

--- aviary/roost/model.py
+++ aviary/roost/model.py
@@ -226,9 +226,9 @@ class DescriptorNetwork(nn.Module):
             for attn_head in self.cry_pool
         ]
 
-        if 0 in cry_elem_idx:
-            print(f'elem_fea: {elem_fea}')
-            print(f'cry_elem_idx: {cry_elem_idx}')
+        # if 0 in cry_elem_idx:
+        #     print(f'elem_fea: {elem_fea}')
+        #     print(f'cry_elem_idx: {cry_elem_idx}')
             
         # if pred_idx:
             
